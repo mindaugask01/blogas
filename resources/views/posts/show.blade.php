d34dcab44bb4d9f2f694faacbfc115b95a7bcf4a
mindaugask01 <mindaugask01@gmail.com>
2017-03-31 22:36:26 +0300

    Part 15 - Working with Dates from Database + edit delete mygtuku kurimas

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,34 @@
 
 
 @section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <h1>{{ $post->title }}</h1>
+            <p class="lead">{{ $post->body }}</p>
+        </div>
+        <div class="col-md-4">
+            <div class="well">
+                <dl class="dl-horizontal">
+                    <dt>Created at:</dt>
+                    <dd>{{ $post->created_at }}</dd>
+                </dl>
 
-    <h1>{{ $post->title }}</h1>
-    <p class="lead">{{ $post->body }}</p>
+                <dl class="dl-horizontal">
+                    <dt>Last updated:</dt>
+                    <dd>{{ $post->updated_at }}</dd>
+                </dl>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary btn-block">Edit</a>
+                    </div>
 
+                    <div class="col-sm-6">
+                        <a href="{{ route('posts.destroy', $post->id) }}" class="btn btn-danger btn-block">Delete</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 @stop
